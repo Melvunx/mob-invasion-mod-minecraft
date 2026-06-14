a2546ebb912ad3b564774c60cb9253eec20f79a9
Melvunx <augustine.melv@gmmail.com>
2026-06-14 19:25:25 +0200

    init commit

---
+++ src/client/java/melvunx/mobinvasion/net/client/MobInvasionClient.java
@@ -0,0 +1,10 @@
+package melvunx.mobinvasion.net.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MobInvasionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
